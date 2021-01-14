6c749b74c208b3ca97bb026884cc6168f7371a49
jcrusam <jcrusam11@gmail.com>
2021-01-14 03:40:13 +0000

    feat(Items): add Items enum and populate

---
+++ app/src/main/java/com/devfox/items/Items.java
@@ -0,0 +1,22 @@
+package com.devfox.items;
+
+public enum Items {
+    IRON_ORE,
+    IRON_INGOT,
+    COPPER_ORE,
+    COPPER_INGOT,
+    IRON_PLATE,
+    IRON_ROD,
+    SCREW,
+    COPPER_SHEET,
+    WIRE,
+    CABLE,
+    CONCRETE,
+    SILICA,
+    BIOMASS,
+    SOLID_BIOFUEL,
+    REINFORCED_IRON_PLATE,
+    MODULAR_FRAME,
+    ROTOR,
+    SMART_PLATING
+}
