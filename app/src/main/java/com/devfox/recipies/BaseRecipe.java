e92cecc10703c01d386dbb90fd9bdc7df72bca3e
jcrusam <jcrusam11@gmail.com>
2021-01-14 03:57:07 +0000

    feat(Recipe): add output count to class

--- app/src/main/java/com/devfox/recipies/BaseRecipe.java
+++ app/src/main/java/com/devfox/recipies/BaseRecipe.java
@@ -6,7 +6,7 @@ import com.devfox.items.Items;
  * A recipe with no inputs
  */
 public class BaseRecipe extends Recipe{
-    public BaseRecipe(String name, Items outputItem){
-        super(name,null,outputItem);
+    public BaseRecipe(String name, Items outputItem,float outputCountPerMinute){
+        super(name,null,outputItem,outputCountPerMinute);
     }
 }
--- app/src/main/java/com/devfox/recipies/Recipe.java
+++ app/src/main/java/com/devfox/recipies/Recipe.java
@@ -6,16 +6,19 @@ public class Recipe {
     private Recipe[] inputRecipes;
     private String name;
     private Items outputItem;
+    private float outputCountPerMinute; //How many of the output item are produced per minute
+
     /**
      * Constructor for creating a recipe that takes a set of input recipes and
      * @param name
      * @param inputRecipes
      * @param outputItem
      */
-    public Recipe(String name, Recipe[] inputRecipes, Items outputItem){
+    public Recipe(String name, Recipe[] inputRecipes, Items outputItem,float outputCountPerMinute ){
         this.inputRecipes = inputRecipes;
         this.name = name;
         this.outputItem = outputItem;
+        this.outputCountPerMinute = outputCountPerMinute;
     }
 
     public String getName(){
@@ -28,5 +31,9 @@ public class Recipe {
 
     public Recipe[] getInputRecipes(){
         return inputRecipes;
+    }
+
+    public float getOutputCountPerMinute(){
+        return outputCountPerMinute;
     }
 }
