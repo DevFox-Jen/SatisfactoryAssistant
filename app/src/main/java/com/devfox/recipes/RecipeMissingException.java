fcc603619deb4726bc96b2e53b1946a276b945e7
jcrusam <jcrusam11@gmail.com>
2021-01-17 20:42:22 +0000

    feat(recipes): remove unnecessary responsibilities from Recipe and remove deprecated classes

--- app/src/main/java/com/devfox/recipes/RecipeMissingException.java
+++
@@ -1,7 +0,0 @@
-package com.devfox.recipes;
-
-public class RecipeMissingException extends RuntimeException{
-    public RecipeMissingException(String itemMissingRecipe){
-        super("Recipe missing for: " + itemMissingRecipe);
-    }
-}
