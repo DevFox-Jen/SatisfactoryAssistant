c9fbf62457a5f632b136b571ebf04ed13d7938f5
jcrusam <jcrusam11@gmail.com>
2021-01-17 14:12:04 +0000

    feat(PipelineFactory): change PipelineFactory to be an abstract factory

--- app/src/main/java/com/devfox/production/pipeline/PipelineFactory.java
+++ app/src/main/java/com/devfox/production/pipeline/PipelineFactory.java
@@ -3,20 +3,16 @@ package com.devfox.production.pipeline;
 import com.devfox.SatisfactoryAssistant.MethodNotImplementedException;
 import com.devfox.recipes.Recipe;
 
-public class PipelineFactory {
-    public enum Type{
-        XML
-    }
+public abstract class PipelineFactory {
     /**
      * Attempts to construct a pipeline to produce the target item at the target output rate
      * @param recipes The set of recipes to use for creating the pipeline. If there are duplicate recipes then the builder will use the first recipe it finds
      * @param targetItem The output item to produce
      * @param targetOutputPerMin The amount of the item to produce per minute
-     * @param pipelineType how the underlying pipeline representation is implemented
      * @return The root pipeline node for the resulting pipeline
      */
-    public PipelineNode buildPipeline(Recipe[] recipes, String targetItem, float targetOutputPerMin,Type pipelineType){
-
+    public PipelineNode buildPipeline(Recipe[] recipes, String targetItem, float targetOutputPerMin){
+        throw new MethodNotImplementedException();
     }
 
     /**
@@ -25,10 +21,9 @@ public class PipelineFactory {
      * @param targetItem The item to produce
      * @param limitingInputItem The item who's input rate is limited
      * @param limitingItemInputPerMin The input limit of the limiting item
-     * @param pipelineType how the underlying pipeline representation is implemented
      * @return the root pipeline node for the resulting pipeline
      */
-    public PipelineNode buildPipeline(Recipe[] recipes, String targetItem, String limitingInputItem, float limitingItemInputPerMin,Type pipelineType){
+    public PipelineNode buildPipeline(Recipe[] recipes, String targetItem, String limitingInputItem, float limitingItemInputPerMin){
         throw new MethodNotImplementedException();
     }
 }
