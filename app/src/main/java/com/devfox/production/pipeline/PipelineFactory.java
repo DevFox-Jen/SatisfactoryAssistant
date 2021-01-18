4faaba13cd8939f57164cc10fed44663774637e7
jcrusam <jcrusam11@gmail.com>
2021-01-18 00:01:34 +0000

    feat(PipelineFactory): implement PipelineFactory to pass all current tests

--- app/src/main/java/com/devfox/production/pipeline/PipelineFactory.java
+++ app/src/main/java/com/devfox/production/pipeline/PipelineFactory.java
@@ -3,8 +3,9 @@ package com.devfox.production.pipeline;
 import com.devfox.SatisfactoryAssistant.MethodNotImplementedException;
 import com.devfox.items.ItemStack;
 import com.devfox.recipes.Recipe;
+import org.checkerframework.checker.units.qual.A;
 
-import java.util.Map;
+import java.util.*;
 
 /**
  * The PipelineFactory aims to create a tree in the form of chained {@link PipelineNode}.
@@ -35,13 +36,60 @@ public abstract class PipelineFactory {
      * Attempts to construct a pipeline to produce the set target item. if {@link #setTargetOutputPerMinute} has not been set, then the pipeline will aim to produce as much of the set target item as possible.
      * @param recipes The set of recipes to use for creating the pipeline. If there are duplicate recipes then the builder will use the first recipe it finds to produce an item.
      * @return The root pipeline node for the resulting pipeline
+     * @throws RecipeMissingException if one or more required recipes are missing
      */
-    public PipelineNode buildPipeline(Recipe[] recipes){
-        throw new MethodNotImplementedException();
+    public PipelineNode buildPipeline(Recipe[] recipes) throws RecipeMissingException{
+        checkAllRequiredRecipesExist(recipes,getOutputItem());
+        return onBuildPipeline(recipes);
+    }
+
+    /**
+     * Performs a search of the recipe list using the target item as the starting point to check that every recipe required to create the output item exists in the list
+     * It will not stop when one missing recipe is found. It will report the recipes missing from each branch of the pipeline in the form of the thrown exception
+     * It cannot report recipes that are missing where there is no transitive relationship.
+     * E.g. if we take the iron plate recipe: Miner -> Iron Ore -> Iron Ingot -> Iron Plate, and remove Iron Ore -> Iron Ingot
+     * The function cannot infer that Iron Ore -> Iron Ingot is missing, it can only state that there is no recipe present to craft Iron Ingots to enable the recipe Iron Ingot -> Iron Plate
+     * @param recipes The set of recipes to check
+     * @param targetItem The item to check the recipes are present for
+     * @throws RecipeMissingException if one or more recipes are missing from the list.
+     */
+    public static void checkAllRequiredRecipesExist(Recipe[] recipes, String targetItem) throws RecipeMissingException{
+        Queue<String> itemLookupQueue = new LinkedList<>();
+        HashMap<String, Recipe> recipeHashMap = new HashMap<>();
+        List<String> itemsMissingRecipes = new ArrayList<>();
+
+        for(Recipe recipe : recipes){
+            //Make an entry using the recipe output item id as the key, and the recipe as the value
+            recipeHashMap.put(recipe.getOutputItemStack().getItemID(),recipe);
+        }
+
+        //Add the target item as the first item in the queue
+        itemLookupQueue.add(targetItem);
+
+        while(!itemLookupQueue.isEmpty()){
+            String nextItem = itemLookupQueue.poll();
+            Recipe recipeForItem = recipeHashMap.get(nextItem);
+            if(recipeForItem == null){
+                itemsMissingRecipes.add(nextItem);
+            }else{
+                itemLookupQueue.addAll(Arrays.asList(getNamesOfItems(recipeForItem.getInputItemStacks())));
+            }
+        }
+        if(itemsMissingRecipes.size() > 0){
+            throw new RecipeMissingException(itemsMissingRecipes.toArray(new String[0]));
+        }
+    }
+
+    private static String[] getNamesOfItems(ItemStack[] itemStacks){
+        ArrayList<String> arrayList = new ArrayList<>();
+        for(ItemStack itemStack : itemStacks)
+            arrayList.add(itemStack.getItemID());
+        return arrayList.toArray(new String[0]);
     }
 
     /**
      * To be implemented by the extending class. Called when {@link #buildPipeline} is called.
+     * It will only be called if the recipe set has been checked. This means this method will only be called if {@link #checkAllRequiredRecipesExist} returns true on the set of recipes
      * @param recipes The set of recipes to use for creating the pipeline. If there are duplicate recipes then the builder will use the first recipe it finds to produce an item.
      * @return The root pipeline node for the resulting pipeline
      */
